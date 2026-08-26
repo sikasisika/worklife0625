50452095a61ac18b5da2282c4ccfd526061520d6
sikasisika <bleach715_0621@yahoo.co.jp>
2026-08-26 19:29:36 +0900

    Add basic offline caching setup in sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+// オフラインキャッシュ用の基本設定
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
